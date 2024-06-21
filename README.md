b0d104e207d36062c5db0ccee0c75295d6242117
JLesn <lesnikova.j00@gmail.com>
2024-06-21 20:02:22 +0500

    add README an fix mistakes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+#Анализатор страниц
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/JLesn/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/JLesn/python-project-83/actions)
 
@@ -6,3 +8,26 @@
 
 
 https://page-analyzer-31an.onrender.com
+
+
+
+Это веб-сервис, позволяющий проверить сайты на SEO-пригодность путем сбора SEO-тэгов.
+Для проверки нужно ввести URL сайта на главной странице.
+Проверенные сайты выводятся в виде таблицы на вкладке "Сайты".
+
+##Как установить:
+Склонировать репозиторий
+```
+https://github.com/JLesn/python-project-83.gi
+```
+В корневой папке выполнить команду:
+```
+make install
+```
+
+Создать ".env" файл и указать ваш SECRET_KEY и базу данных.
+
+```
+export DATABASE_URL={provider}://{user}:{password}@{host}:{port}/{db} 
+export SECRET_KEY= # your SECRET_KEY
+```
--- page_analyzer/database.py
+++ page_analyzer/database.py
@@ -17,73 +17,95 @@ def connection():
 
 
 def add_to_db(url):
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute(
-                """
-                INSERT INTO urls (name, created_at)
-                VALUES (%s, %s) RETURNING id;
-                """,
-                (url, date.today().strftime("%Y-%m-%d")),
-            )
-            id = cur.fetchone().id
-            return id
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute(
+                    """
+                    INSERT INTO urls (name, created_at)
+                    VALUES (%s, %s) RETURNING id;
+                    """,
+                    (url, date.today().strftime("%Y-%m-%d")),
+                )
+                id = cur.fetchone().id
+                return id
+    finally:
+        conn.close()
 
 
 def find_by_id(id):
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute("""SELECT * FROM urls WHERE id = %s;""", (id,))
-            return cur.fetchone()
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute("""SELECT * FROM urls WHERE id = %s;""", (id,))
+                return cur.fetchone()
+    finally:
+        conn.close()
 
 
 def find_by_url(url):
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute("""SELECT * FROM urls WHERE name = %s;""", (url,))
-            return cur.fetchone()
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute("""SELECT * FROM urls WHERE name = %s;""", (url,))
+                return cur.fetchone()
+    finally:
+        conn.close()
 
 
 def get_all_from_db():
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute("""SELECT * FROM urls ORDER BY id DESC;""")
-            return cur.fetchall()
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute("""SELECT * FROM urls ORDER BY id DESC;""")
+                return cur.fetchall()
+    finally:
+        conn.close()
 
 
 def make_check(id):
     url = find_by_id(id).name
     res = check_html(url)
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute(
-                """
-                INSERT INTO url_checks
-                (url_id, status_code, h1, title, description, created_at)
-                VALUES (%s, %s, %s, %s, %s, %s);
-                """,
-                (id, res['status_code'], res['h1'],
-                    res['title'], res['description'],
-                    date.today().strftime("%Y-%m-%d")),)
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute(
+                    """
+                    INSERT INTO url_checks
+                    (url_id, status_code, h1, title, description, created_at)
+                    VALUES (%s, %s, %s, %s, %s, %s);
+                    """,
+                    (id, res['status_code'], res['h1'],
+                        res['title'], res['description'],
+                        date.today().strftime("%Y-%m-%d")),)
+    finally:
+        conn.close()
 
 
 def get_checks(id):
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute(("""SELECT * FROM url_checks WHERE url_id=%s"""), (id,))
-            return cur.fetchall()
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute(("""SELECT * FROM url_checks
+                             WHERE url_id=%s"""), (id,))
+                return cur.fetchall()
+    finally:
+        conn.close()
 
 
 def get_short_info():
-    with connection() as conn:
-        with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
-            cur.execute("""SELECT urls.id,
-                                  urls.name,
-                                  MAX(url_checks.created_at) AS created_at,
-                                  url_checks.status_code
-                         FROM urls
-                         LEFT JOIN url_checks ON
-                                  urls.id = url_checks.url_id
-                         GROUP BY urls.id, url_checks.status_code
-                         ORDER BY urls.id DESC;""")
-            return cur.fetchall()
+    try:
+        with connection() as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
+                cur.execute("""SELECT urls.id,
+                                      urls.name,
+                                      MAX(url_checks.created_at) AS created_at,
+                                      url_checks.status_code
+                             FROM urls
+                             LEFT JOIN url_checks ON
+                                      urls.id = url_checks.url_id
+                             GROUP BY urls.id, url_checks.status_code
+                             ORDER BY urls.id DESC;""")
+                return cur.fetchall()
+    finally:
+        conn.close()
